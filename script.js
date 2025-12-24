a3f75a46ef43a63a41fc7e21e9e1ea073fd266f5
Doniyor Baxtiyorovich <qwdonyor@gmail.com>
2025-12-24 17:51:28 +0500

    Increase number of elements and update styles

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-for(let i=0;i<55;i++){
+for(let i=0;i<120;i++){
     let qor = document.createElement("div");
     qor.className = "qor";
-    qor.innerHTML = ".";
+    qor.innerHTML = "❅";
     qor.style.left = Math.random()*100 + "vw";
-    qor.style.animationDuration = 3 + Math.random()*2 + "s";
+    qor.style.animationDuration = 3 + Math.random()*6 + "s";
     document.body.appendChild(qor);
 }
+setTimeout(() => {
+  document.body.classList.add("blur"); // 4 sekunddan keyin blur qo‘shadi
+}, 1400);
+setTimeout(() => {
+    document.getElementById("logo").classList.add("jsanim");
+}, 1700);
+setTimeout(() => {
+    document.getElementById("navbar").classList.add("jsanim");
+}, 1700);
+
